dd98512125c510f8f2618f6106954d1bb135f5c0
EricWu <jianhuawu2000@foxmail.com>
2025-03-20 22:08:08 +0800

    feat: add headers in HuaEngine.h

--- HuaEngine/src/HuaEngine.h
+++ HuaEngine/src/HuaEngine.h
@@ -3,5 +3,10 @@
 #include "HuaEngine/Application.h"
 #include "HuaEngine/Core/Log.h"
 #include "HuaEngine/Core/Layer.h"
+#include "HuaEngine/Core/Input.h"
+
 #include "HuaEngine/GUI/ImguiLayer.h"
+
 #include "HuaEngine/EntryPoint.h"  
+
+#include "imgui.h"
