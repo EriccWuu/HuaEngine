1c649e9a8aafa03e9b2fbb103314557736705b1b
EricWu <jianhuawu2000@foxmail.com>
2025-03-20 22:08:45 +0800

    docs: project file changes

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <HuaEngine.h>
 
+class CustomLayer : public HE::Layer {
+public:
+	CustomLayer(): Layer("CumsomLayer") {
+
+	}
+
+	void OnUpdate() override {
+
+	}
+
+	void OnGuiRender() override {
+		ImGui::Begin("Test");
+		ImGui::Text("Hello");
+		ImGui::End();
+	}
+};
+
 class SandboxApp : public HE::Application {
 public:
 	SandboxApp() {
-
+		PushLayer(new CustomLayer());
 	}
 
 	~SandboxApp() {
