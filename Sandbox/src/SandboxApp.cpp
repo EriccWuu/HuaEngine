3b87fa4f107eca8ad6eb047fdef51631238ecfbe
EricWu <jianhuawu2000@foxmail.com>
2025-03-26 20:45:16 +0800

    feat: Implemented window docking and framebuffer in sandbox

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -9,24 +9,30 @@ using namespace HE;
 class CustomLayer : public HE::Layer {
 public:
 	CustomLayer(): Layer("CumsomLayer") {
+        
+	}
+
+    void OnAttach() override {
+        // Create vertex buffer
 		float squareVertices[4 * 5] = {
-			-0.8f, -0.8f, 0.0f, 0.0 , 0.0,
-			 0.8f, -0.8f, 0.0f, 1.0 , 0.0,
-			 0.8f,  0.8f, 0.0f, 1.0 , 1.0,
-			-0.8f,  0.8f, 0.0f, 0.0 , 1.0
+			-0.5f, -0.5f, 0.0f, 0.0 , 0.0,
+			 0.5f, -0.5f, 0.0f, 1.0 , 0.0,
+			 0.5f,  0.5f, 0.0f, 1.0 , 1.0,
+			-0.5f,  0.5f, 0.0f, 0.0 , 1.0
 		};
-
-		unsigned int squareIndices[6] = {
-			0, 1, 2, 2, 3, 0
-		};
-
-		m_SquareVA = VertexArray::Create();
 
 		Ref<VertexBuffer> squareVertexBuffer;
 		squareVertexBuffer = VertexBuffer::Create(squareVertices, sizeof(squareVertices));
+
+        // Create index buffer
+        unsigned int squareIndices[6] = {
+            0, 1, 2 , 2, 3, 0
+        };
+
 		Ref<IndexBuffer> squareIndexBuffer;
 		squareIndexBuffer = IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t));
 
+        // Create vertex array
 		BufferLayout squareLayout = {
 			{ ShaderDataType::Float3, "aPosition" },
 			{ ShaderDataType::Float2, "aTexCoord" }
@@ -34,9 +40,11 @@ public:
 
 		squareVertexBuffer->SetLayout(squareLayout);
 
+        m_SquareVA = VertexArray::Create();
 		m_SquareVA->AddVertexBuffer(squareVertexBuffer);
 		m_SquareVA->SetIndexBuffer(squareIndexBuffer);
 
+        // Create shaders
 		std::string squareVS = R"(
 			#version 330 core
 
@@ -64,6 +72,7 @@ public:
 
 			void main() {
 				FragColor = texture(uTexture, vTexCoord);
+                // FragColor = vec4(vTexCoord, 0.0, 0.0);
 			}
 		)";
 
@@ -72,21 +81,152 @@ public:
 		m_Texture = Texture2D::Create("assets/textures/hutao.png");
 		m_Texture->Bind(0);
 		std::dynamic_pointer_cast<OpenGLShader>(m_SquareShader)->UploadUniformInt("uTexture", 0);
-	}
+
+        // Create framebuffer
+        FrameBufferSpecification spec;
+        spec.Width = 1280;
+        spec.Height = 720;
+        spec.Attachments = { FrameBufferTextureFormat::RGBA8 };
+        m_FrameBuffer = FrameBuffer::Create(spec);
+    }
 
 	void OnUpdate() override {
-		Renderer::Begin();
+        Renderer::Begin();
+        m_FrameBuffer->Bind();
+
+        RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
+        RenderCommand::Clear();
+
+        m_Texture->Bind(0);
+        std::dynamic_pointer_cast<OpenGLShader>(m_SquareShader)->UploadUniformInt("uTexture", 0);
 
 		m_SquareShader->Bind();
 		Renderer::Submit(m_SquareVA);
-
+        
+        m_FrameBuffer->Unbind();
 		Renderer::End();
+	}
+
+	void OnGuiRender() override {
+        // READ THIS !!!
+        // TL;DR; this demo is more complicated than what most users you would normally use.
+        // If we remove all options we are showcasing, this demo would become:
+        //     void ShowExampleAppDockSpace()
+        //     {
+        //         ImGui::DockSpaceOverViewport(0, ImGui::GetMainViewport());
+        //     }
+        // In most cases you should be able to just call DockSpaceOverViewport() and ignore all the code below!
+        // In this specific demo, we are not using DockSpaceOverViewport() because:
+        // - (1) we allow the host window to be floating/moveable instead of filling the viewport (when opt_fullscreen == false)
+        // - (2) we allow the host window to have padding (when opt_padding == true)
+        // - (3) we expose many flags and need a way to have them visible.
+        // - (4) we have a local menu bar in the host window (vs. you could use BeginMainMenuBar() + DockSpaceOverViewport()
+        //      in your code, but we don't here because we allow the window to be floating)
+
+        static bool enable_docking = true;
+
+        static bool opt_fullscreen = true;
+        static bool opt_padding = false;
+        static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+        // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+        // because it would be confusing to have two docking targets within each others.
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+        if (opt_fullscreen)
+        {
+            const ImGuiViewport* viewport = ImGui::GetMainViewport();
+            ImGui::SetNextWindowPos(viewport->WorkPos);
+            ImGui::SetNextWindowSize(viewport->WorkSize);
+            ImGui::SetNextWindowViewport(viewport->ID);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+            window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+            window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+        }
+        else
+        {
+            dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+        }
+
+        // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+        // and handle the pass-thru hole, so we ask Begin() to not render a background.
+        if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+            window_flags |= ImGuiWindowFlags_NoBackground;
+
+        // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+        // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+        // all active windows docked into it will lose their parent and become undocked.
+        // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+        // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+        if (!opt_padding)
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("DockSpace Demo", &enable_docking, window_flags);
+        if (!opt_padding)
+            ImGui::PopStyleVar();
+
+        if (opt_fullscreen)
+            ImGui::PopStyleVar(2);
+
+        // Submit the DockSpace
+        ImGuiIO& io = ImGui::GetIO();
+        if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+        {
+            ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+            ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+        }
+        else
+        {
+            HE_ASSERT(false, "Docking is no enabled!");
+        }
+
+        if (ImGui::BeginMenuBar())
+        {
+            if (ImGui::BeginMenu("Options"))
+            {
+                // Disabling fullscreen would allow the window to be moved to the front of other windows,
+                // which we can't undo at the moment without finer window depth/z control.
+                ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen);
+                ImGui::MenuItem("Padding", NULL, &opt_padding);
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Flag: NoDockingOverCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingOverCentralNode) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingOverCentralNode; }
+                if (ImGui::MenuItem("Flag: NoDockingSplit", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingSplit) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingSplit; }
+                if (ImGui::MenuItem("Flag: NoUndocking", "", (dockspace_flags & ImGuiDockNodeFlags_NoUndocking) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoUndocking; }
+                if (ImGui::MenuItem("Flag: NoResize", "", (dockspace_flags & ImGuiDockNodeFlags_NoResize) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoResize; }
+                if (ImGui::MenuItem("Flag: AutoHideTabBar", "", (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar; }
+                if (ImGui::MenuItem("Flag: PassthruCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0, opt_fullscreen)) { dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode; }
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Close", NULL, false, &enable_docking != NULL))
+                    enable_docking = false;
+                ImGui::EndMenu();
+            }
+
+            ImGui::EndMenuBar();
+        }
+
+        ImGui::End();
+
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
+        ImGui::Begin("Scene");
+        ImVec2 scenePanelSize = ImGui::GetContentRegionAvail();
+        if (glm::vec2{ scenePanelSize.x, scenePanelSize.y } != m_SceneViewportSize)
+            m_FrameBuffer->Resize((uint32_t)scenePanelSize.x, (uint32_t)scenePanelSize.y);
+        m_SceneViewportSize = { scenePanelSize.x, scenePanelSize.y };
+        ImGui::Image(m_FrameBuffer->GetColorAttachment(), 
+            { m_SceneViewportSize.x , m_SceneViewportSize.y },
+            {0, 1}, {1, 0});
+        ImGui::End();
+        ImGui::PopStyleVar();
 	}
 
 private:
 	Ref<Shader> m_SquareShader;
 	Ref<VertexArray> m_SquareVA;
 	Ref<Texture2D> m_Texture;
+    Ref<FrameBuffer> m_FrameBuffer;
+
+    glm::vec2 m_SceneViewportSize = {0, 0};
 };
 
 class SandboxApp : public HE::Application {
