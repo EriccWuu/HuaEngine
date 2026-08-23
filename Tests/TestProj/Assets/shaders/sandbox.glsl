2b361576ad2684a4a81a4548f8333f1b17a10161
EricWu <jianhuawu2000@163.com>
2026-08-23 13:18:02 +0800

    fix(testproj): sample sandbox material texture

--- Tests/TestProj/Assets/shaders/sandbox.glsl
+++ Tests/TestProj/Assets/shaders/sandbox.glsl
@@ -25,7 +25,5 @@ uniform vec4 u_Color;
 uniform sampler2D u_Texture;
 
 void main() {
-    // FragColor = texture(u_Texture, v_TexCoord);
-    // FragColor = vec4(v_TexCoord, 0.0, 1.0);
-    FragColor = u_Color;
+    FragColor = texture(u_Texture, v_TexCoord);
 }
